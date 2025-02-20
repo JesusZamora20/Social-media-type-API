ab91ff40f3feb15192f5340e36ceb3343b22eac5
JESUS DAVID BERMUDEZ ZAMORA <jbermudezamora87@gmail.com>
2025-02-20 00:48:20 -0500

    post method

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Body
+from fastapi.params import Body
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return{"message":"Hello FastAPI"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"This is your post"}
+
+@app.post("/created")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"newpost": f"{payload['title']}, content -> {payload['content']}"}
